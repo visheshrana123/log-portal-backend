6c3aa2a68b9a70d1d799eb5ae9551c0320b188e2
VISHESH RANA <153714748+visheshrana123@users.noreply.github.com>
2026-04-26 14:48:42 +0530

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LogPortalBackend.Models;
+
+namespace LogPortalBackend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<log> Logs { get; set; }
+    }
+}
