1fc95a878c2d6111f1c272c633314702b82f61a0
VISHESH RANA <153714748+visheshrana123@users.noreply.github.com>
2026-04-26 14:47:59 +0530

    Add files via upload

---
+++ Models/log.cs
@@ -0,0 +1,11 @@
+﻿namespace LogPortalBackend.Models
+{
+    public class log
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public string Level { get; set; } // INFO, ERROR
+        public DateTime Timestamp { get; set; }
+
+    }
+}
